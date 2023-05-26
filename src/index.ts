3c86603aa9a1a955ef880ba747695325b509e2d2
lukasz.szczesniak <lukasz.szczesniak@theheart.dev>
2023-05-26 10:27:36 +0200

    fix: adjust for stackblitz

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,18 @@
 import fastify from "fastify";
 import pino from "pino";
+import pretty from "pino-pretty";
 import fastifyws from "@fastify/websocket";
 import { onConnection } from "./connections";
 import { commandHandler } from "./commands/handler";
 import { registerCommands } from "./commands/strategies";
 
 const server = fastify({
-  logger: pino({ level: "trace" }),
+  logger: pino(
+    { level: "trace" },
+    pretty({
+      colorize: true,
+    })
+  ),
 });
 
 server.register(fastifyws);
@@ -15,7 +21,7 @@ server.register(async (fastify) => {
   fastify.get("/", { websocket: true }, onConnection);
 });
 
-server.listen({ port: 8080 }, (err) => {
+server.listen({ port: 3010 }, (err) => {
   if (err) {
     process.exit(1);
   }
